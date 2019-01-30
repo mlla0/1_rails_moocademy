31153c91c52c9c2456d22ee95f41d3c35834495c
mlla <maud@mlav.land>
2019-01-30 15:25:50 +0100

    add of two models and db: courses and lessons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Course.destroy_all
+Lesson.destroy_all
+
+20.times do |index|
+	course = Course.create(id: index + 1, title: Faker::GreekPhilosophers.name, content: Faker::GreekPhilosophers.quote)
+end
+
+60.times do |index|
+	c = Course.find(1 + rand(20))
+	l = Lesson.new(id: index + 1, title: Faker::Job.title, body: Faker::Job.education_level)
+	l.course = c
+	l.save
+end
